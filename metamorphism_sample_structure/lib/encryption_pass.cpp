5b3afddd307562f24005e76682b1e3bd23474d82
Philogex <64011915+Philogex@users.noreply.github.com>
2024-12-27 07:54:28 -0500

    fixed build process (finished); update pass to transform with noop

--- metamorphism_sample_structure/lib/encryption_pass.cpp
+++ metamorphism_sample_structure/lib/encryption_pass.cpp
@@ -1,41 +1,36 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/GlobalVariable.h"
-#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Intrinsics.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Passes/PassPlugin.h"
 
 using namespace llvm;
 
 namespace {
-
-    // As described in: https://sh4dy.com/2024/07/06/learning_llvm_02/
-    void funp1(Module &M) {
-        auto globals = M.globals();
-
-        for(auto itr = globals.begin(); itr != globals.end(); itr++){
-            StringRef varName = itr->getName();
-            Type* ty = itr->getType();
-            errs() << "Variable Name: " << varName << "\n";
-            errs() << "Variable Type: ";
-            ty->print(errs());
-            errs() << "\n";
+    void noop(Module &M) {
+        // Iterate over all functions in the module
+        for (Function &F : M) {
+            continue;
         }
     }
 
-    // New PM implementation
-    struct EncryptPass : PassInfoMixin<EncryptPass> {
+    // New PM implementation for a transformation pass
+    struct EncryptionPass : PassInfoMixin<EncryptionPass> {
         // Main entry point, takes IR unit to run the pass on (&M) and the
-        // corresponding pass manager (to be queried if need be)
+        // corresponding pass manager (to be queried if needed)
         PreservedAnalyses run(Module &M, ModuleAnalysisManager &MPM) {
-            funp1(M);
-            return PreservedAnalyses::all();
+            // Perform the transformation (replace 'add' with 'sub')
+            noop(M);
+
+            // Indicate that the module was transformed
+            return PreservedAnalyses::none(); // Module is transformed (no-op analysis)
         }
 
-        // Without isRequired returning true, this pass will be skipped for functions
-        // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
-        // all functions with optnone.
+        // This pass will not be skipped for functions decorated with optnone
         static bool isRequired() { return true; }
     };
 
@@ -46,12 +41,12 @@ namespace {
 // New PM Registration
 //-----------------------------------------------------------------------------
 PassPluginLibraryInfo getMyPassPluginInfo() {
-    return {LLVM_PLUGIN_API_VERSION, "EncryptPass", LLVM_VERSION_STRING, //callback following
+    return {LLVM_PLUGIN_API_VERSION, "EncryptionPass", LLVM_VERSION_STRING, // callback following
         [](PassBuilder &PB) { // instantiate
             PB.registerPipelineParsingCallback(
             [&](StringRef name, ModulePassManager &MPM, ArrayRef<PassBuilder::PipelineElement>) {
-                if (name == "encrypt-pass") {
-                    MPM.addPass(EncryptPass());
+                if (name == "encryption-pass") {
+                    MPM.addPass(EncryptionPass());
                     return true;
                 }
                 return false;
